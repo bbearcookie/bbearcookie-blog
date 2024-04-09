7ba7d944965eb61e9caf84891569883384b5484d
bbearcookie <aodem@naver.com>
2024-04-09 17:09:05 +0900

    feat: 이미지 가운데 정렬 처리

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps) => <NextImage className="mx-auto" {...rest} />
 
 export default Image
