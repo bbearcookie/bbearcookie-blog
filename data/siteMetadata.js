18a2a76ac009ba832c6f72d32aeca174ebf74921
bbearcookie <aodem@naver.com>
2024-04-10 18:44:49 +0900

    chore: ga id 추가

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -38,9 +38,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'G-TQMKLDZC1K', // e.g. G-XXXXXXX
+    },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
