3c090fb5e6f9de5fe183c14c0ed269b22312ff2f
bbearcookie <aodem@naver.com>
2024-04-14 20:49:29 +0900

    design: 이미지와 caption 사이의 불편한 마진 제거

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,19 +2,17 @@ import NextImage, { ImageProps } from 'next/image'
 import Link from 'next/link'
 
 const Image = ({ ...props }: ImageProps) => {
-  const { src, alt, width, height } = props
+  const { src, alt } = props
 
   const queryString = new URLSearchParams({
     src: src as string,
     alt,
-    width: width?.toString() || '',
-    height: height?.toString() || '',
   }).toString()
 
   return (
     <figure>
       <Link href={`/image?${queryString}`} target="_blank">
-        <NextImage className="mx-auto" {...props} />
+        <NextImage className="mx-auto my-0" {...props} />
       </Link>
       <figcaption className="text-center">{alt}</figcaption>
     </figure>
