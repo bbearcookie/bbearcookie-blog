caa1c902e688438720b0c3cda0f30d60703637c2
bbearcookie <aodem@naver.com>
2024-04-23 16:26:14 +0900

    design: Blog, Tag 관련 페이지 레이아웃 수정

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -79,28 +79,79 @@ export default function ListLayoutWithTags({
 
   return (
     <>
-      <div>
-        <div className="pb-6 pt-6">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:hidden sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {title}
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="mb-4">
+          <h1 className="text-xl font-extrabold uppercase leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-2xl md:leading-14">
+            {pathname.split('/tags/')[1]
+              ? `${pathname.split('/tags/')[1]} 와 관련된 ${posts.length}개의 포스트`
+              : `전체 ${posts.length}개의 포스트`}
           </h1>
         </div>
         <div className="flex sm:space-x-24">
+          <div className="grow">
+            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+              {displayPosts.map((post) => {
+                const { path, date, title, summary, tags, slug } = post
+                return (
+                  <li key={path} className="py-10">
+                    <article className="flex flex-col space-y-2 xl:space-y-0">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                        </dd>
+                      </dl>
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-10 tracking-tight">
+                            <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {tags?.map((tag) => <Tag key={tag} text={tag} />)}
+                          </div>
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                        <div className="text-base font-medium leading-6">
+                          <Link
+                            href={`/blog/${slug}`}
+                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                            aria-label={`Read more: "${title}"`}
+                          >
+                            Read more &rarr;
+                          </Link>
+                        </div>
+                      </div>
+                    </article>
+                  </li>
+                )
+              })}
+            </ul>
+            {pagination && pagination.totalPages > 1 && (
+              <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
+            )}
+          </div>
           <div className="hidden h-full min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
             <div className="px-6 py-4">
-              {pathname.startsWith('/blog') ? (
-                <h3 className="font-bold uppercase text-primary-500">
-                  All Posts ({totalPostCount})
-                </h3>
-              ) : (
-                <Link
-                  href={`/blog`}
-                  className="font-bold uppercase text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
-                >
-                  All Posts ({totalPostCount})
-                </Link>
-              )}
+              <h3 className="font-bold uppercase text-gray-700 dark:text-gray-300">Tag List</h3>
               <ul>
+                <li className="my-3">
+                  {pathname.startsWith('/blog') ? (
+                    <h3 className="inline px-3 py-2 text-sm font-bold uppercase text-primary-500">
+                      all ({totalPostCount})
+                    </h3>
+                  ) : (
+                    <Link
+                      href={`/blog`}
+                      className="px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
+                    >
+                      all ({totalPostCount})
+                    </Link>
+                  )}
+                </li>
                 {sortedTags.map((t) => {
                   return (
                     <li key={t} className="my-3">
@@ -122,43 +173,6 @@ export default function ListLayoutWithTags({
                 })}
               </ul>
             </div>
-          </div>
-          <div>
-            <ul>
-              {displayPosts.map((post) => {
-                const { path, date, title, summary, tags } = post
-                return (
-                  <li key={path} className="py-5">
-                    <article className="flex flex-col space-y-2 xl:space-y-0">
-                      <dl>
-                        <dt className="sr-only">Published on</dt>
-                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                        </dd>
-                      </dl>
-                      <div className="space-y-3">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags?.map((tag) => <Tag key={tag} text={tag} />)}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                    </article>
-                  </li>
-                )
-              })}
-            </ul>
-            {pagination && pagination.totalPages > 1 && (
-              <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
-            )}
           </div>
         </div>
       </div>
