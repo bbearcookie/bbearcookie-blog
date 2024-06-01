bc67d657de33f671cd1554bd7b35a58bb07f3d64
bbearcookie <aodem@naver.com>
2024-06-02 00:39:52 +0900

    chore: 구글 서치 콘솔을 위한 메타 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="google-site-verification" content="eIIp9NlMF9MkZ9ya4BPiDiPsnAD6qqMSBn6OG5D4zcs" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
